0930992c9a9f76d2e627368fa45fff0c7513c8ab
cmd08010 <colleendunion@gmail.com>
2020-01-22 21:16:48 -0500

    a to b finished date thingy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Grid from './Components/Grid';
-import Slider from './Components/Slider';
-import ThisWeek from './Components/ThisWeek';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Grid from "./Components/Grid"
+import Slider from "./Components/Slider"
+import ThisWeek from "./Components/ThisWeek"
+import moment from "moment"
 
 //GRID CODE
 function App() {
@@ -16,41 +17,127 @@ function App() {
     false,
     false,
     false,
-    false,
-  ]);
+    false
+  ])
 
   const handleClickBox = e => {
-    console.log(e.target.innerText);
+    console.log(e.target.innerText)
 
-    let currentIndex = e.target.innerText;
+    let currentIndex = e.target.innerText
 
     if (grid[currentIndex] === false) {
-      grid[currentIndex] = true;
+      grid[currentIndex] = true
     } else {
-      grid[currentIndex] = false;
+      grid[currentIndex] = false
     }
-    setGrid([...grid]);
-  };
+    setGrid([...grid])
+  }
 
   // SLIDER CODE
 
-  let [colorNumber, setColorNumber] = useState([0, 0, 0]);
+  let [colorNumber, setColorNumber] = useState([0, 0, 0])
 
   const handleSlider = e => {
-    let newColor = [...colorNumber];
-    if (e.target.id === 'rSlider') {
-      newColor[0] = e.target.value;
-    } else if (e.target.id === 'gSlider') {
-      newColor[1] = e.target.value;
-    } else if (e.target.id === 'bSlider') {
-      newColor[2] = e.target.value;
+    let newColor = [...colorNumber]
+    if (e.target.id === "rSlider") {
+      newColor[0] = e.target.value
+    } else if (e.target.id === "gSlider") {
+      newColor[1] = e.target.value
+    } else if (e.target.id === "bSlider") {
+      newColor[2] = e.target.value
     }
-    setColorNumber(newColor);
-  };
+    setColorNumber(newColor)
+  }
 
   // THISWEEK CODE
 
-  let [week, setWeek] = useState([0, 1, 2, 3, 4, 5, 6]);
+  let [startDate, setStartDate] = useState(moment("01/19/2020").toDate())
+
+  let weekArray = [
+    startDate.toString(),
+    moment(startDate)
+      .add(1, "days")
+      .toString(),
+    moment(startDate)
+      .add(2, "days")
+      .toString(),
+    moment(startDate)
+      .add(3, "days")
+      .toString(),
+    moment(startDate)
+      .add(4, "days")
+      .toString(),
+    moment(startDate)
+      .add(5, "days")
+      .toString(),
+    moment(startDate)
+      .add(6, "days")
+      .toString()
+  ]
+
+  //  console.log(weekArray)
+  let [week, setWeek] = useState([...weekArray])
+
+  const previousWeek = e => {
+    let newStartDate = moment(startDate)
+      .subtract(7, "days")
+      .toString()
+
+    let previousWeekArray = [
+      newStartDate,
+      moment(newStartDate)
+        .add(1, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(2, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(3, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(4, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(5, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(6, "days")
+        .toString()
+    ]
+    console.log(previousWeekArray)
+    setStartDate(newStartDate)
+    setWeek([...previousWeekArray])
+  }
+  const nextWeek = e => {
+    let newStartDate = moment(startDate)
+      .add(7, "days")
+      .toString()
+    console.log(newStartDate)
+    let previousWeekArray = [
+      newStartDate,
+      moment(newStartDate)
+        .add(1, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(2, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(3, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(4, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(5, "days")
+        .toString(),
+      moment(newStartDate)
+        .add(6, "days")
+        .toString()
+    ]
+    console.log(previousWeekArray)
+    setStartDate(newStartDate)
+    setWeek([...previousWeekArray])
+  }
 
   return (
     <div className="App">
@@ -61,12 +148,16 @@ function App() {
       <div id="thisWeek">
         <ThisWeek week={week} />
         <div id="weekButtons">
-          <button id="previous">Previous Week</button>
-          <button id="next">Next Week</button>
+          <button id="previous" onClick={previousWeek}>
+            Previous Week
+          </button>
+          <button id="next" onClick={nextWeek}>
+            Next Week
+          </button>
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/ThisWeek.js
+++ src/Components/ThisWeek.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react"
 
 function ThisWeek(props) {
-  return props.week.map(function(day) {
-    return <div></div>;
-  });
+  return props.week.map(function(day, index) {
+    return (
+      <div className="day" key={index}>
+        {day}
+      </div>
+    )
+  })
 }
 
-export default ThisWeek;
+export default ThisWeek
