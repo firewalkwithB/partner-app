6d97d7f71d654e34f660ba020e186069a08d749d
cmd08010 <colleendunion@gmail.com>
2020-01-22 20:14:49 -0500

    A to B : finished color slider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Grid from './Components/Grid';
-import Slider from './Components/Slider';
+import React, { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Grid from "./Components/Grid"
+import Slider from "./Components/Slider"
 
 //GRID CODE
 function App() {
@@ -15,34 +15,46 @@ function App() {
     false,
     false,
     false,
-    false,
-  ]);
+    false
+  ])
 
   const handleClickBox = e => {
-    console.log(e.target.innerText);
+    console.log(e.target.innerText)
 
-    let currentIndex = e.target.innerText;
+    let currentIndex = e.target.innerText
 
     if (grid[currentIndex] === false) {
-      grid[currentIndex] = true;
+      grid[currentIndex] = true
     } else {
-      grid[currentIndex] = false;
+      grid[currentIndex] = false
     }
-    setGrid([...grid]);
-  };
+    setGrid([...grid])
+  }
 
   // SLIDER CODE
 
-  let [colorNumber, setColorNumber] = useState([0, 0, 0]);
+  let [colorNumber, setColorNumber] = useState([0, 0, 0])
+
+  const handleSlider = e => {
+    let newColor = [...colorNumber]
+    if (e.target.id === "rSlider") {
+      newColor[0] = e.target.value
+    } else if (e.target.id === "gSlider") {
+      newColor[1] = e.target.value
+    } else if (e.target.id === "bSlider") {
+      newColor[2] = e.target.value
+    }
+    setColorNumber(newColor)
+  }
 
   return (
     <div className="App">
       <div id="grid">
         <Grid grid={grid} handleClickBox={handleClickBox} />
       </div>
-      <Slider colorNumber={colorNumber} />
+      <Slider colorNumber={colorNumber} handleSlider={handleSlider} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
